573af8518ef672600e2d56d056c0534b90df4f63
FruitsL <sa4560@naver.com>
2022-08-09 16:54:14 +0900

    logout 수정
    postmapping 추가

--- src/main/java/com/ktds/zipzero/member/MemberController.java
+++ src/main/java/com/ktds/zipzero/member/MemberController.java
@@ -2,20 +2,21 @@ package com.ktds.zipzero.member;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j2;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/member")
 @Log4j2
 public class MemberController {
     @GetMapping("/login")
     public void login(){
-        
+
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public void logout(){
 
     }
