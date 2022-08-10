cd8d0b1ff9c4ab52c9d581ff79883d368af5f100
Ganbilly <94830725+Ganbilly@users.noreply.github.com>
2022-08-11 08:48:53 +0900

    임시저장

--- src/main/java/com/ktds/zipzero/payment/service/PaymentServiceImpl.java
+++ src/main/java/com/ktds/zipzero/payment/service/PaymentServiceImpl.java
@@ -13,7 +13,7 @@ import lombok.extern.log4j.Log4j2;
 
 /*
  * 만든 사람 : 정문경(2022-08-10)
- * 최종 수정 : 정문경(2022-08-10)
+ * 최종 수정 : 이은성(2022-08-11)
  * 기능 : 페이지 맵퍼
  */
 
@@ -33,5 +33,15 @@ public class PaymentServiceImpl implements PaymentService {
     public PaymentDTO getPaymentDetail(long pid) {
         PaymentDTO ppayment = paymentMapper.getPaymentById(pid);
         return ppayment;
+    }
+
+    /*
+    * 만든 사람 : 이은성(2022-08-11)
+    * 최종 수정 : 이은성(2022-08-11)
+    * 기능 : 페이먼트 등록
+    */
+    @Override
+    public void registPayment(PaymentDTO paymentDTO) {
+        paymentMapper.registPayment(paymentDTO);
     }
 }
